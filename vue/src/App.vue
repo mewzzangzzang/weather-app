2f4336d9a077971bda6177a6d99a62f2451398e8
ze <uzzz7899@gmail.com>
2025-01-24 02:55:07 +0900

    스캐폴딩, HTML 작성

---
+++ vue/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import { onBeforeMount, onBeforeUnmount } from 'vue';
+
+// css height 100vh적용시 웹 브라우저 상관없이 항상 일정한 100vh 값
+const init = () => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+      };
+      onBeforeMount(() => {
+        init();
+        window.addEventListener("resize", init);
+      });
+      onBeforeUnmount(()=> {
+        window.removeEventListener("resize", init);
+      })
+</script>
+<template>
+  <RouterView />
+</template>
---
+++ vue/src/views/SearchView.vue
@@ -0,0 +1,55 @@
+<script setup></script>
+<template>
+   <main class="weather-city">
+      <!-- 검색 영역 -->
+      <section class="weather__search">
+        <input
+          type="text"
+          class="weather__searchBar"
+          placeholder="도시를 검색해주세요"
+        />
+      </section>
+      <!-- 검색 데이터가 있으면 -->
+      <section class="weather__city">
+        <div class="weather__cityLeft">
+          <strong class="weather__cityTmp">25°</strong>
+          <p class="weather__cityTmpMore">H: 27° / L: 19°</p>
+          <p>Montral, Canada</p>
+        </div>
+        <div class="weather__cityRight">
+          <img
+            src="@/assets/images/icons/cloudy.png"
+            alt=""
+            class="weather__cityImg"
+          />
+        </div>
+        <span class="material-symbols-outlined weather__cancel"> cancel </span>
+      </section>
+      <!-- 검색 데이터가 없으면 -->
+      <section class="no-data" style="display: none">
+        <p>검색한 지역이 없습니다.</p>
+      </section>
+    </main>
+    <!-- 공통 하단 영역 -->
+    <footer class="footer">
+      <ul class="footer__list">
+        <li class="footer__item">
+          <a href="/">
+            <span class="material-symbols-outlined footer_icons"> home </span>
+          </a>
+        </li>
+        <li>
+          <a href="forecast.html">
+            <span class="material-symbols-outlined footer_icons">
+              stacked_line_chart
+            </span>
+          </a>
+        </li>
+        <li>
+          <a href="/search.html">
+            <span class="material-symbols-outlined footer_icons"> search </span>
+          </a>
+        </li>
+      </ul>
+    </footer>
+</template>
