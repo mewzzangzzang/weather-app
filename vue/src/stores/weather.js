bcd7a5c886d5f7e020cab16a42e355cb6cfcd590
ze <uzzz7899@gmail.com>
2025-02-05 00:33:52 +0900

    api 연동 (-오류코드 429) 으로 홈 화면 기능구현현

--- vue/src/stores/weather.js
+++ vue/src/stores/weather.js
@@ -1,2 +1,29 @@
 import { defineStore } from "pinia";
-export const useWeatherStore = defineStore('weather',() => {});
+import axios from "axios";
+import { ref } from "vue";
+
+const axiosIstance = axios.create({
+    baseURL:'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline',
+    params: {
+        lang:'ko',
+        key:'-------',
+        unitGroup:'metric',
+    },
+});
+
+export const useWeatherStore = defineStore('weather', () => {
+    //초기 검색 지역
+    const address = ref('seoul');
+    //현재 날씨 정보 데이터
+    const currentConditions = ref(null);
+    //현재 날씨 API 불러오기
+    const getCurrentWeatherInfo = async () => {
+        try {
+            const res = await axiosIstance.get('/' + address.value);
+            currentConditions.value = res.data.currentConditions;
+        } catch (e) {
+            alert(e.resonse?.data ? e.resonse?.data : e.message);
+        }
+    };
+    return { currentConditions, getCurrentWeatherInfo, };
+})
